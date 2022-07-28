730910ce693b2d4f3ec750d6649e82e87cbcd9e6
Sergey <Flargoin@gmail.com>
2022-07-28 14:32:21 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$('.slider__inner').slick({
+  prevArrow: `<button type="button" class="slick-prev"><img src="icons/arrow-left.svg" alt="arrow-left"></button>`,
+  nextArrow: `<button type="button" class="slick-next"><img src="icons/arrow-right.svg" alt="arrow-right"></button>`
+});
